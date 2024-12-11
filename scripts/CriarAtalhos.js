dc2969ced539c305d1f3b838494a23da7f8da498
RavenaStar <ravenastar@proton.me>
2024-12-11 15:35:04 -0300

    ⚙️

--- scripts/CriarAtalhos.js
+++ scripts/CriarAtalhos.js
@@ -4,19 +4,19 @@ var CurrentDirectory = WshShell.CurrentDirectory;
 var shortcuts = [
     { shortcut: "WINTOOLS MOUSE (HTA).lnk", target: "tools\\mouse\\menu.hta", root: true },
     { shortcut: "WINTOOLS TECLADO (CMD).lnk", target: "tools\\menu.cmd", root: true },
-    { shortcut: "tools\\mouse\\reboot_normal.lnk", target: "tools\\mouse\\reboot_normal.cmd", root: false },
-    { shortcut: "tools\\mouse\\reboot_bios.lnk", target: "tools\\mouse\\reboot_bios.cmd", root: false },
-    { shortcut: "tools\\mouse\\shutdown.lnk", target: "tools\\mouse\\shutdown.cmd", root: false },
-    { shortcut: "tools\\mouse\\task_manager.lnk", target: "tools\\mouse\\task_manager.cmd", root: false },
-    { shortcut: "tools\\mouse\\lock_screen.lnk", target: "tools\\mouse\\lock_screen.cmd", root: false },
-    { shortcut: "tools\\mouse\\open_appsfolder.lnk", target: "tools\\mouse\\open_appsfolder.cmd", root: false },
-    { shortcut: "tools\\mouse\\god_mode.lnk", target: "tools\\mouse\\god_mode.cmd", root: false },
-    { shortcut: "tools\\mouse\\clean_temp_files.lnk", target: "tools\\mouse\\clean_temp_files.cmd", root: false },
-    { shortcut: "tools\\mouse\\enable_f8.lnk", target: "tools\\mouse\\enable_f8.cmd", root: false },
-    { shortcut: "tools\\mouse\\disable_f8.lnk", target: "tools\\mouse\\disable_f8.cmd", root: false },
-    { shortcut: "tools\\mouse\\create_restore_point.lnk", target: "tools\\mouse\\create_restore_point.cmd", root: false },
-    { shortcut: "tools\\mouse\\enable_unlimited_restore_points.lnk", target: "tools\\mouse\\enable_unlimited_restore_points.cmd", root: false },
-    { shortcut: "tools\\mouse\\open_site.lnk", target: "tools\\mouse\\open_site.cmd", root: false }
+    { shortcut: "tools\\mouse\\[A]reboot_normal.lnk", target: "tools\\mouse\\reboot_normal.cmd", root: false },
+    { shortcut: "tools\\mouse\\[A]reboot_bios.lnk", target: "tools\\mouse\\reboot_bios.cmd", root: false },
+    { shortcut: "tools\\mouse\\[A]shutdown.lnk", target: "tools\\mouse\\shutdown.cmd", root: false },
+    { shortcut: "tools\\mouse\\[A]task_manager.lnk", target: "tools\\mouse\\task_manager.cmd", root: false },
+    { shortcut: "tools\\mouse\\[A]lock_screen.lnk", target: "tools\\mouse\\lock_screen.cmd", root: false },
+    { shortcut: "tools\\mouse\\[A]open_appsfolder.lnk", target: "tools\\mouse\\open_appsfolder.cmd", root: false },
+    { shortcut: "tools\\mouse\\[A]god_mode.lnk", target: "tools\\mouse\\god_mode.cmd", root: false },
+    { shortcut: "tools\\mouse\\[A]clean_temp_files.lnk", target: "tools\\mouse\\clean_temp_files.cmd", root: false },
+    { shortcut: "tools\\mouse\\[A]enable_f8.lnk", target: "tools\\mouse\\enable_f8.cmd", root: false },
+    { shortcut: "tools\\mouse\\[A]disable_f8.lnk", target: "tools\\mouse\\disable_f8.cmd", root: false },
+    { shortcut: "tools\\mouse\\[A]create_restore_point.lnk", target: "tools\\mouse\\create_restore_point.cmd", root: false },
+    { shortcut: "tools\\mouse\\[A]enable_unlimited_restore_points.lnk", target: "tools\\mouse\\enable_unlimited_restore_points.cmd", root: false },
+    { shortcut: "tools\\mouse\\[A]open_site.lnk", target: "tools\\mouse\\open_site.cmd", root: false }
 ];
 
 for (var i = 0; i < shortcuts.length; i++) {
