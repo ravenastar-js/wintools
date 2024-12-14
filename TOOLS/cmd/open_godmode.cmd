4d9121d194eab9178a893655cf4193b075041c03
RavenaStar <ravenastar@proton.me>
2024-12-14 13:49:32 -0300

    ⚙️

--- TOOLS/cmd/open_godmode.cmd
+++ TOOLS/cmd/open_godmode.cmd
@@ -1,4 +1,11 @@
 @echo off
-color 0A
-mkdir "%userprofile%\Desktop\GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}"
-explorer "%userprofile%\Desktop\GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}"
+set tempDir=%TEMP%\GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}
+if exist "%tempDir%" (
+    echo A pasta existe. Abrindo...
+    explorer "%tempDir%"
+) else (
+    echo Criando a pasta God Mode...
+    mkdir "%tempDir%"
+    explorer "%tempDir%"
+)
+exit /b
